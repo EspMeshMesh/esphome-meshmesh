e8d185248cb8e0ea2c0a883090173ee120b7c2d2
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2025-10-04 07:53:26 +0200

    espmeshmesh v1.2.1

--- components/meshmesh/__init__.py
+++ components/meshmesh/__init__.py
@@ -75,7 +75,7 @@ async def to_code(config):
 
     cg.add_library(
         name="ESPMeshMesh",
-        version="1.1.2",
+        version="1.2.1",
         repository="persuader72/ESPMeshMesh",
     )
 
