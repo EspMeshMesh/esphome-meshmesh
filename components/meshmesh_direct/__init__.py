9c89db9809a98d17599c218405556fa8fac9425a
Stefano Pagnottelli <stefano.pagnottelli@siralab.com>
2026-03-03 10:03:37 +0100

    fix coroutine priority for meshmesh_direct

--- components/meshmesh_direct/__init__.py
+++ components/meshmesh_direct/__init__.py
@@ -4,6 +4,8 @@ from esphome.components.udp import CONF_ON_RECEIVE
 import esphome.config_validation as cv
 from esphome.const import CONF_ADDRESS, CONF_DATA, CONF_ID, CONF_TRIGGER_ID
 from esphome.types import ConfigType
+from esphome.core import coroutine_with_priority
+from esphome.coroutine import CoroPriority
 
 CODEOWNERS = ["@persuader72"]
 DEPENDENCIES = ["meshmesh"]
@@ -63,6 +65,7 @@ async def _trigger_to_code(config):
     return trigger
 
 
+@coroutine_with_priority(CoroPriority.NETWORK_SERVICES)
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
