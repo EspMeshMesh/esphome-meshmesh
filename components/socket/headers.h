9b734bee1fdd512918a91d5c8b7ca709709fa971
Stefano Pagnottelli <stefano@pagnottelli.it>
2026-09-19 07:09:59 +0200

    component socket ported to esphome 2026.7.4

--- components/socket/headers.h
+++ components/socket/headers.h
@@ -104,7 +104,7 @@ struct iovec {
   size_t iov_len;
 };
 
-#if defined(USE_ESP8266) || defined(USE_RP2040)
+#if defined(USE_ESP8266) || defined(USE_RP2)
 // arduino-esp8266 declares a global vars called INADDR_NONE/ANY which are invalid with the define
 #ifdef INADDR_ANY
 #undef INADDR_ANY
@@ -158,7 +158,9 @@ using socklen_t = uint32_t;
 #include <sys/ioctl.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#ifndef USE_ZEPHYR
 #include <sys/uio.h>
+#endif
 #include <unistd.h>
 
 #ifdef USE_HOST
@@ -167,6 +169,10 @@ using socklen_t = uint32_t;
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #endif  // USE_HOST
+#ifdef USE_ZEPHYR
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#endif  // USE_ZEPHYR
 
 #ifdef USE_ARDUINO
 // arduino-esp32 declares a global var called INADDR_NONE which is replaced
@@ -185,7 +191,6 @@ using socklen_t = uint32_t;
 #endif
 
 #endif  // USE_SOCKET_IMPL_BSD_SOCKETS
-
 
 // Start Meshmesh implementation -->
 #if defined(USE_SOCKET_IMPL_MESHMESH_ESP32)
